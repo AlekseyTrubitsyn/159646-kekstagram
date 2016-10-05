f820b78399d9f301e130bbdc4c40457be7c03f92
Aleksey <at.8441451@yandex.ru>
2016-10-05 19:26:41 +0300

    Начинаем программировать (#2)
    * done
    
    * bugs fixed
    
    * set strict equality
    
    * fix array multiplication

---
+++ src/js/check.js
@@ -0,0 +1,30 @@
+function getMessage(a, b) {
+  if (typeof a === "boolean") {
+    if (a) {
+      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";      
+    } else {
+      return "Переданное GIF-изображение не анимировано";      
+    }
+  }
+
+  if (typeof a === "number") {
+    return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
+  } 
+
+  if (Array.isArray(a) && !Array.isArray(b)) {
+    var amountOfRedPoints = a.reduce(function (sum, current) {
+      return sum + current;
+    });
+    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var artifactsSquare = a.reduce(function (sum, currentItem, index) {      
+      return (currentItem * b[index]) + sum;
+    }, 0);
+
+    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+  }
+
+  return "Переданы некорректные данные";
+}
