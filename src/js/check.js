27c1eba59bebc5124e6e365f0cc1f5ccb90a4cf3
Aleksey Trubtisyn <at.8441451@yandex.ru>
2016-10-08 00:40:26 +0300

    fix bugs in check.js

--- src/js/check.js
+++ src/js/check.js
@@ -1,30 +1,32 @@
+'use strict';
+
 function getMessage(a, b) {
-  if (typeof a === "boolean") {
+  if (typeof a === 'boolean') {
     if (a) {
-      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";      
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
     } else {
-      return "Переданное GIF-изображение не анимировано";      
+      return 'Переданное GIF-изображение не анимировано';
     }
   }
 
-  if (typeof a === "number") {
-    return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " атрибутов";
-  } 
+  if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
+  }
 
   if (Array.isArray(a) && !Array.isArray(b)) {
-    var amountOfRedPoints = a.reduce(function (sum, current) {
+    var amountOfRedPoints = a.reduce(function(sum, current) {
       return sum + current;
     });
-    return "Количество красных точек во всех строчках изображения: " + amountOfRedPoints;
+    return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   }
 
   if (Array.isArray(a) && Array.isArray(b)) {
-    var artifactsSquare = a.reduce(function (sum, currentItem, index) {      
+    var artifactsSquare = a.reduce(function(sum, currentItem, index) {
       return (currentItem * b[index]) + sum;
     }, 0);
 
-    return "Общая площадь артефактов сжатия: " + artifactsSquare + " пикселей";
+    return 'Общая площадь артеф актов сжатия: ' + artifactsSquare + ' пикселей';
   }
 
-  return "Переданы некорректные данные";
+  return 'Переданы некорректные данные';
 }
